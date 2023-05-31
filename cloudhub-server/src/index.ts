f33a754beed3b5086865e2b4c91375aa9692f683
Michael Liendo <michael.m.liendo.r@gmail.com>
2023-05-31 14:57:02 -0400

    feat(server): rome config and lint

--- cloudhub-server/src/index.ts
+++ cloudhub-server/src/index.ts
@@ -1,17 +1,17 @@
-import * as dotenv from 'dotenv';
+import * as dotenv from "dotenv";
 dotenv.config();
 
-import Fastify from 'fastify';
+import Fastify from "fastify";
 
 const fastify = Fastify();
 
 fastify.listen(
-  { port: Number(process.env.PORT) || 3000 },
-  function (err, address) {
-    if (err) {
-      console.error(err);
-      process.exit(1);
-    }
-    console.log(`Server is now listening on ${address}`);
-  }
+	{ port: Number(process.env.PORT) || 3000 },
+	function (err, address) {
+		if (err) {
+			console.error(err);
+			process.exit(1);
+		}
+		console.log(`Server is now listening on ${address}`);
+	},
 );
