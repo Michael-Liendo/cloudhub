738b9814c1cabb6062cae296437550dbd3f2ec96
Michael Liendo <michael.m.liendo.r@gmail.com>
2023-06-02 19:24:27 -0400

    feat(client): sign up

--- cloudhub/src/lib/components/TextField.svelte
+++ cloudhub/src/lib/components/TextField.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
   import classNames from 'classnames';
 
-  export let type: 'text' | 'number' | 'email' | 'password' | 'date' = 'text';
+  export let type: 'text' | 'number' | 'email' | 'password' | 'username' =
+    'text';
   export let name: string;
   export let id = name;
   export let error: string | null = null;
@@ -31,26 +32,59 @@
   >
 {/if}
 
-<input
-  {name}
-  {id}
-  {placeholder}
-  {required}
-  {autocomplete}
-  {type}
-  {value}
-  class:bg-gray-700={error}
-  class:border-red-500={error}
-  class:text-red-900={error}
-  class:placeholder-red-500={error}
-  class:focus:ring-red-500={error}
-  class:focus:border-red-500={error}
-  class={className}
-  on:change
-  on:blur
-  on:focus
-  on:input={handleInput}
-/>
+{#if type == 'username'}
+  <div class="flex">
+    <span
+      class="inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 border border-r-0 border-gray-300 rounded-l-md dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600"
+    >
+      @
+    </span>
+    <input
+      {name}
+      {id}
+      {placeholder}
+      {required}
+      {autocomplete}
+      {type}
+      {value}
+      class:rounded-none={type}
+      class:rounded-r-lg={type}
+      class:bg-gray-700={error}
+      class:border-red-500={error}
+      class:text-red-900={error}
+      class:placeholder-red-500={error}
+      class:focus:ring-red-500={error}
+      class:focus:border-red-500={error}
+      class={className}
+      on:change
+      on:blur
+      on:focus
+      on:input={handleInput}
+    />
+  </div>
+{:else}
+  <input
+    {name}
+    {id}
+    {placeholder}
+    {required}
+    {autocomplete}
+    {type}
+    {value}
+    class:bg-gray-700={error}
+    class:border-red-500={error}
+    class:text-red-900={error}
+    class:placeholder-red-500={error}
+    class:focus:ring-red-500={error}
+    class:focus:border-red-500={error}
+    class={className}
+    on:change
+    on:blur
+    on:focus
+    on:input={handleInput}
+  />
+{/if}
+
 {#if error}
   <p class="mt-2 text-sm text-red-500">
     {error}
---
+++ cloudhub/src/routes/(auth)/signup/+page.svelte
@@ -0,0 +1,63 @@
+<script lang="ts">
+  import Button from '$lib/components/Button.svelte';
+  import TextField from '../../../lib/components/TextField.svelte';
+</script>
+
+<h1 class="font-medium text-4xl md:text-5xl">Sign Up</h1>
+<!-- TODO: values -->
+<form class="mt-10">
+  <div class="grid gap-6 mb-6 md:grid-cols-2">
+    <div>
+      <TextField
+        label="First Name"
+        name="name"
+        type="text"
+        placeholder="John"
+        required
+      />
+    </div>
+    <div>
+      <TextField
+        label="Last name"
+        name="last_name"
+        type="text"
+        placeholder="Doe"
+        required
+      />
+    </div>
+  </div>
+  <div class="mb-6">
+    <TextField
+      label="Username"
+      name="username"
+      type="username"
+      placeholder="johndoe"
+      required
+    />
+  </div>
+  <div class="mb-6">
+    <TextField
+      label="Email address"
+      name="email"
+      type="email"
+      placeholder="john.doe@company.com"
+      required
+    />
+  </div>
+  <div class="mb-6">
+    <TextField
+      label="Password"
+      name="password"
+      type="password"
+      placeholder="* * * * * * *"
+      required
+    />
+  </div>
+
+  <Button type="submit" fullWidth>Sign up</Button>
+</form>
+
+<div class="text-right">
+  <span>Don't have an account?</span>
+  <a href="/login" class="font-medium text-blue-500 hover:underline">Log in</a>
+</div>
