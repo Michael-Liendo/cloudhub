b3fa0792098427c5d79c0992c4d1abd14e729cfc
Michael Liendo <michael.m.liendo.r@gmail.com>
2023-06-04 15:54:21 -0400

    feat(server): better register

--- cloudhub-server/src/controllers/auth/register.ts
+++ cloudhub-server/src/controllers/auth/register.ts
@@ -4,6 +4,7 @@ import { hashPassword } from "../../helpers/hashPassword";
 import { createUser } from "../../domain/repository/user/createUser";
 import generateToken from "../../helpers/generateToken";
 import { userSchema } from "../../domain/model/user";
+import { checkExistingUser } from "../../domain/repository/user/checkExistingUser";
 
 export default async function registerControllers(
 	request: FastifyRequest,
@@ -13,21 +14,40 @@ export default async function registerControllers(
 		const { first_name, last_name, username, email, password } =
 			request.body as CreateUserRequest;
 
-		if (!first_name || !email || !password) {
-			throw {
-				statusCode: 400,
-				message: "Missing fields first_name, email and password",
-				error: "Bad Request",
-			};
+		let errorFields = [];
+
+		await userSchema
+			.validate(request.body, { abortEarly: false })
+			.catch((err) => {
+				errorFields = err.inner.map((fieldError) => ({
+					field: fieldError.path,
+					message: fieldError.errors[0],
+				}));
+			});
+		const existingUser = await checkExistingUser(username, email);
+
+		if (existingUser.username) {
+			errorFields.push({
+				field: "username",
+				message: "Username already exists.",
+			});
 		}
 
-		await userSchema.validate({ first_name, email, password }).catch((err) => {
+		if (existingUser.email) {
+			errorFields.push({
+				field: "email",
+				message: "Email already exists.",
+			});
+		}
+
+		if (errorFields.length > 0) {
 			throw {
 				statusCode: 400,
-				message: err.message,
+				message: "Existing user error",
 				error: "Bad Request",
+				errorFields,
 			};
-		});
+		}
 
 		const hashedPassword = await hashPassword(password);
 
@@ -39,48 +59,21 @@ export default async function registerControllers(
 			password: hashedPassword,
 		};
 
-		try {
-			const userCreation = await createUser(user);
+		const accessToken = await generateToken(await createUser(user));
 
-			const accessToken = await generateToken(userCreation);
-
-			response.setCookie("accessToken", accessToken).status(200).send({
-				statusCode: 200,
-				error: null,
-				data: {
-					accessToken,
-				},
-				success: true,
-			});
-		} catch (error) {
-			if (error.code === "23505") {
-				const regex = /\(([^)]+)\)=(.*)/g;
-				const [, column] = regex.exec(error.detail);
-
-				throw {
-					statusCode: 400,
-					error: "Bad Request",
-					message: `${column} already exists`,
-				};
-			} else if (error.code === "23502") {
-				throw {
-					statusCode: 400,
-					error: "Bad Request",
-					message: "A field is missing",
-				};
-			} else {
-				console.error(error);
-				throw {
-					statusCode: 500,
-					error: "Internal Server Error",
-					message: error.message,
-				};
-			}
-		}
+		response.setCookie("accessToken", accessToken).status(200).send({
+			statusCode: 200,
+			error: null,
+			data: {
+				accessToken,
+			},
+			success: true,
+		});
 	} catch (error) {
 		response.code(error.statusCode || 500).send({
 			statusCode: error.statusCode || 500,
 			error: {
+				...error,
 				message: error.message || "Internal Server Error",
 				error: error.error || "Internal Server Error",
 			},
