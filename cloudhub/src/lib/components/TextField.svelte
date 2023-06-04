09de62b3f7cba253ee33b98a7fb4e6940d27f261
Michael Liendo <michael.m.liendo.r@gmail.com>
2023-06-04 08:22:52 -0400

    feat(client): text pasword view and hidden password

--- cloudhub/src/lib/components/TextField.svelte
+++ cloudhub/src/lib/components/TextField.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
   import classNames from 'classnames';
+  import EyeClosed from '~icons/mdi/eye-closed';
+  import EyeOpen from '~icons/mdi/eye';
+
   // TODO: especial password input with a view a hidden text
   export let type: 'text' | 'number' | 'email' | 'password' | 'username' =
     'text';
@@ -19,6 +22,11 @@
     customClassNames,
     'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
   );
+
+  let viewPassword: boolean;
+  function changeInputView() {
+    viewPassword = !viewPassword;
+  }
 
   const handleInput = (event: Event): void => {
     const target = event.target as HTMLInputElement;
@@ -61,6 +69,40 @@
       on:focus
       on:input={handleInput}
     />
+  </div>
+{:else if type == 'password'}
+  <div class="relative mb-6">
+    <input
+      {name}
+      {id}
+      {placeholder}
+      {required}
+      {autocomplete}
+      type={viewPassword ? 'text' : type}
+      {value}
+      class:bg-gray-700={error}
+      class:border-red-500={error}
+      class:text-red-900={error}
+      class:placeholder-red-500={error}
+      class:focus:ring-red-500={error}
+      class:focus:border-red-500={error}
+      class={className}
+      on:change
+      on:blur
+      on:focus
+      on:input={handleInput}
+    />
+    <button
+      type="button"
+      on:click={changeInputView}
+      class="transition-all duration-1000 absolute inset-y-0 right-0 flex items-center pr-3"
+    >
+      {#if viewPassword}
+        <EyeOpen />
+      {:else}
+        <EyeClosed />
+      {/if}
+    </button>
   </div>
 {:else}
   <input
