89eab7b991ab948c36685f81dadfe643f0ba5f59
Michael Liendo <michael.m.liendo.r@gmail.com>
2023-06-04 16:03:51 -0400

    feat:

--- cloudhub/src/routes/(auth)/signup/+page.svelte
+++ cloudhub/src/routes/(auth)/signup/+page.svelte
@@ -7,24 +7,57 @@
 
   const { handleSubmit, values, errors, isSubmitting } = newForm({
     initialValues: {
-      name: '',
-      lastName: '',
+      first_name: '',
+      last_name: '',
       username: '',
       email: '',
       password: '',
     },
-    /* todo: better validations */
     validationSchema: Yup.object({
-      name: Yup.string().required(),
-      lastName: Yup.string().required(),
-      username: Yup.string().required(),
-      email: Yup.string().email().required(),
-      password: Yup.string().required(),
+      first_name: Yup.string().required('First name is required.'),
+      last_name: Yup.string().required('Last name is required.'),
+      username: Yup.string()
+        .required('Username is required.')
+        .matches(
+          /^[a-zA-Z0-9_-]+$/,
+          'Username can only contain letters, numbers, hyphens, and underscores.'
+        )
+        .min(3, 'Username must be at least 3 characters.')
+        .max(20, 'Username cannot exceed 20 characters.'),
+      email: Yup.string()
+        .required('Email is required.')
+        .email('Enter a valid email address.'),
+      password: Yup.string()
+        .required('Password is required.')
+        .min(8, 'Password must be at least 8 characters.')
+        .matches(
+          /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^\da-zA-Z]).{8,}$/,
+          'Password must contain lowercase, uppercase, number, and special character.'
+        ),
     }),
-    onSubmit(values, helpers) {
-      setTimeout(() => {
-        console.log(values, helpers);
-      }, 2000);
+    async onSubmit(values, helpers) {
+      const request = await fetch(
+        `${import.meta.env.VITE_API_URL}/api/auth/register`,
+        {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify(values),
+        }
+      );
+      const response = await request.json();
+
+      if (!response.success) console.log(response);
+
+      if (response.statusCode == 400) {
+        response.error?.errorFields.forEach(
+          (error: { field: any; message: string }) => {
+            helpers.setFieldError(error.field, error.message);
+          }
+        );
+      }
+
+      // todo: enter to the platform
+      console.log(response);
     },
   });
 </script>
@@ -39,21 +72,21 @@
     <div>
       <TextField
         label="First Name"
-        name="name"
+        name="first_name"
         type="text"
         placeholder="John"
-        bind:value={$values.name}
-        error={$errors.name}
+        bind:value={$values.first_name}
+        error={$errors.first_name}
       />
     </div>
     <div>
       <TextField
         label="Last Name"
-        name="lastName"
+        name="last_name"
         type="text"
         placeholder="Doe"
-        bind:value={$values.lastName}
-        error={$errors.lastName}
+        bind:value={$values.last_name}
+        error={$errors.last_name}
       />
     </div>
   </div>
@@ -61,7 +94,7 @@
     <TextField
       label="Username"
       name="username"
-      type="username"
+      type="text"
       placeholder="johndoe"
       bind:value={$values.username}
       error={$errors.username}
