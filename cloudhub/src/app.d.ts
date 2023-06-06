df2112e0e8c6af735726c9e28fef76cc467911ab
Michael Liendo <michael.m.liendo.r@gmail.com>
2023-06-06 13:16:22 -0400

    feat: types

--- cloudhub/src/app.d.ts
+++ cloudhub/src/app.d.ts
@@ -1,11 +1,31 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
+
+export type User = {
+	id: string;
+	avatar: "" | string;
+	first_name: string;
+	last_name: string;
+	username: string;
+	email: string;
+	password: string;
+};
+
 declare global {
 	namespace App {
 		// interface Error {}
 		// interface Locals {}
-		// interface PageData {}
-		// interface Platform {}
+		interface PageData {
+			accessToken?: string | null;
+			user?: User | null
+		
+		}
+
+		interface Locals {
+			accessToken?: string | null;
+			user?: User | null
+		}
+
 	}
 }
 
