93c660fb469a3f8011e306d12c92d675b89b348e
Michael Liendo <michael.m.liendo.r@gmail.com>
2023-06-06 15:19:01 -0400

    feat: authenticate page and home

--- cloudhub/src/hooks.server.ts
+++ cloudhub/src/hooks.server.ts
@@ -1,18 +1,15 @@
 import type { Handle } from '@sveltejs/kit';
 
 async function getUserDetails(accessToken: string) {
-	const response = await fetch(`${import.meta.env.VITE_API_URL}/api/users/user/`, {
+	const request = await fetch(`${import.meta.env.VITE_API_URL}/api/users/user/`, {
 		headers: {
 			Authorization: `JWT ${accessToken}`
 		}
 	});
 
-	if (!response.ok) {
-		throw new Error('Failed to fetch user details');
-	}
-	const { data } = await response.json();
+	const response = await request.json();
 
-	return data.user;
+	return response;
 }
 
 
@@ -24,17 +21,23 @@ export const handle: Handle = async ({ event, resolve }) => {
 			return await resolve(event);
 		}
 
-		const userDetails = await getUserDetails(accessToken).catch((error) => {
-			if (error.message === 'Failed to fetch user details') {
-				event.cookies.set('accessToken', '', {
-					maxAge: -1
-				});
-			}
-		});
+		const userDetails = await getUserDetails(accessToken).catch((error) =>{
+			event.cookies.set('accessToken', '', {
+				maxAge: -1
+			});
+			console.error(error);
+			
+		})
+
+		if(!userDetails?.success){
+			event.cookies.set('accessToken', '', {
+				maxAge: -1
+			});
+		}
 
 		if (accessToken) {
 			event.locals.accessToken = accessToken;
-			event.locals.user = userDetails;
+			event.locals.user = userDetails.data.user;
 		}
 
 		return await resolve(event);
