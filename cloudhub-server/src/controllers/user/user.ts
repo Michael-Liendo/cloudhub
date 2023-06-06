427f2c0a369e3a480824c50dfffde4d9680abd0d
Michael Liendo <michael.m.liendo.r@gmail.com>
2023-06-06 13:16:45 -0400

    feat: types

--- cloudhub-server/src/controllers/user/user.ts
+++ cloudhub-server/src/controllers/user/user.ts
@@ -1,6 +1,9 @@
 import { FastifyRequest, FastifyReply } from "fastify";
-import { getUserByUsername } from "../../domain/repository/user/getUser";
-// import jwt from "jsonwebtoken";
+import {
+	getUserById,
+	getUserByUsername,
+} from "../../domain/repository/user/getUser";
+import jwt from "jsonwebtoken";
 
 export default async function userController(
 	request: FastifyRequest,
@@ -9,36 +12,90 @@ export default async function userController(
 	try {
 		const { username } = request.params as { username: string };
 
-		/* const { authorization } = request.headers as { authorization: string };
+		if (username) {
+			const user = await getUserByUsername(username).catch((error) => {
+				if (error.code === "22P02" || error.code === "404") {
+					throw {
+						statusCode: 404,
+						message: "Not found user",
+						error: "Not Found",
+					};
+				} else {
+					throw {
+						statusCode: 500,
+						message: error.detail,
+						error: error.message,
+					};
+				}
+			});
 
-		const token = authorization?.replace("JWT ", "");
+			response.code(200).send({
+				statusCode: 200,
+				data: {
+					user,
+				},
+				error: null,
+				success: true,
+			});
+		}
 
-		const { id: userId } = jwt.verify(token, process.env.JWT_SECRET); */
+		const { authorization } = request.headers as { authorization: string };
 
-		const user = await getUserByUsername(username).catch((error) => {
-			if (error.code === "22P02" || error.code === "404") {
-				throw {
-					statusCode: 404,
-					message: "Not found user",
-					error: "Not Found",
-				};
-			} else {
-				throw {
-					statusCode: 500,
-					message: error.detail,
-					error: error.message,
-				};
-			}
-		});
+		if (authorization) {
+			const token = authorization?.replace("JWT ", "");
 
-		response.code(200).send({
-			statusCode: 200,
-			data: {
-				user,
-			},
-			error: null,
-			success: true,
-		});
+			const decode = jwt.verify(
+				token,
+				process.env.JWT_SECRET,
+				(error, decode) => {
+					if (decode) return decode;
+					else {
+						response.code(401).send({
+							statusCode: 401,
+							error: {
+								message: "Invalid token",
+								error: "Unauthorized",
+							},
+							data: null,
+							success: false,
+						});
+
+						response.setCookie("accessToken", "", {
+							path: "/",
+							secure: true,
+							httpOnly: true,
+							sameSite: "none",
+							domain: "localhost",
+							expires: new Date(),
+						});
+					}
+				},
+			);
+			const user = await getUserById(decode?.id || "").catch((error) => {
+				if (error.code === "22P02" || error.code === "404") {
+					throw {
+						statusCode: 404,
+						message: "Not found user",
+						error: "Not Found",
+					};
+				} else {
+					throw {
+						statusCode: 500,
+						message: error.detail,
+						error: error.message,
+					};
+				}
+			});
+
+			response.code(200).send({
+				statusCode: 200,
+				data: {
+					user,
+				},
+				error: null,
+				success: true,
+			});
+		}
 	} catch (error) {
 		response.code(error.statusCode || 500).send({
 			statusCode: error.statusCode || 500,
--- cloudhub-server/src/helpers/generateToken.ts
+++ cloudhub-server/src/helpers/generateToken.ts
@@ -2,7 +2,7 @@ import jwt from "jsonwebtoken";
 import { User } from "../types/User";
 
 export default function generateToken(user: User) {
-	const payload = { id: user.id, email: user.email };
+	const payload = { id: user.id };
 	const options = { expiresIn: "30d" };
 
 	return jwt.sign(payload, process.env.JWT_SECRET, options);
