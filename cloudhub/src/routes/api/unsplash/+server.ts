d2e7b0683c2e052d310ec9bdcfa64a1ca6a56fdb
Michael Liendo <michael.m.liendo.r@gmail.com>
2023-06-07 08:13:57 -0400

    fix: responsive and background images

--- cloudhub/src/routes/api/unsplash/+server.ts
+++ cloudhub/src/routes/api/unsplash/+server.ts
@@ -34,39 +34,44 @@ export async function GET(event: RequestEvent) {
 		accessKey: process.env.UNSPLASH_ACCESS_KEY || "",
 	});
 
-	const result = await unsplash.search.getPhotos({
+	const result = await unsplash.photos.getRandom({
 		query: "night lights",
 	});
 
 	if (result.status === 200) {
-		const image = result?.response?.results[0];
-		const today = new Date();
+		const image = Array.isArray(result?.response)
+			? result?.response[0]
+			: result?.response;
 
-		today.setDate(today.getDate() + 1);
-		// return the name author
-		const response = {
-			url: image?.urls.regular,
-			author: {
-				id: image?.user.id,
-				username: image?.user.username,
-				url: image?.user.portfolio_url,
-				avatar: image?.user.profile_image.medium,
-			},
-		};
+		if (image) {
+			const today = new Date();
+			today.setDate(today.getDate() + 1);
 
-		event.cookies.set("UNSPLASH_DAILY_BACKGROUND", JSON.stringify(response), {
-			expires: today,
-			path: "/",
-		});
+			const response = {
+				url: image.urls?.regular,
+				author: {
+					id: image.user?.id,
+					username: image.user?.username,
+					name: image.user?.name,
+					url: image.user?.portfolio_url,
+					avatar: image.user?.profile_image?.medium,
+				},
+			};
 
-		return new Response(
-			JSON.stringify({
-				statusCode: 200,
-				data: response,
-				success: true,
-				error: null,
-			}),
-		);
+			event.cookies.set("UNSPLASH_DAILY_BACKGROUND", JSON.stringify(response), {
+				expires: today,
+				path: "/",
+			});
+
+			return new Response(
+				JSON.stringify({
+					statusCode: 200,
+					data: response,
+					success: true,
+					error: null,
+				}),
+			);
+		}
 	}
 
 	return new Response(
