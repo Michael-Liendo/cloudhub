ca29ad1a43704adf8e47ceee043c77e735cf73d5
Michael Liendo <michael.m.liendo.r@gmail.com>
2023-06-11 17:23:01 -0400

    feat: use a svelte-forms

--- cloudhub/src/lib/components/Navbar/Upload.svelte
+++ cloudhub/src/lib/components/Navbar/Upload.svelte
@@ -4,6 +4,7 @@
   import { clickOutside } from '$lib/actions/click_outside';
   import { notifications } from '@whizzes/svelte-notifications';
   import Button from '../Button.svelte';
+  import { newForm } from '@whizzes/svelte-forms';
 
   let isDropdownOpen = false;
   const handleDropdownClick = () => {
@@ -11,33 +12,37 @@
   };
 
   let input: any;
-  async function onSubmit(event: Event) {
-    event.preventDefault();
 
-    let data = new FormData();
-    data.append('file', input.files[0]);
+  const { values, handleSubmit } = newForm({
+    initialValues: {
+      file: null,
+    },
+    async onSubmit() {
+      let data = new FormData();
 
-    const request = await fetch(
-      `${import.meta.env.VITE_API_URL}/api/files/file`,
-      {
-        method: 'POST',
-        headers: {
-          Authorization: `JWT ${$page.data.accessToken}`,
-        },
-        body: data,
+      data.append('file', input.files[0]);
+
+      const request = await fetch(
+        `${import.meta.env.VITE_API_URL}/api/files/file`,
+        {
+          method: 'POST',
+          headers: {
+            Authorization: `JWT ${$page.data.accessToken}`,
+          },
+          body: data,
+        }
+      );
+
+      const response = await request.json();
+
+      if (response.success) {
+        notifications.notifySuccess('File upload!');
+      } else {
+        notifications.notifyFailure(response.error.message || response.error);
+        console.error(response);
       }
-    );
-
-    const response = await request.json();
-
-    if (response.success) {
-      notifications.notifySuccess('File upload!');
-      window.location.href = '/home';
-    } else {
-      notifications.notifyFailure(response.error.message || response.error);
-      console.error(response);
-    }
-  }
+    },
+  });
 </script>
 
 <Button
@@ -52,7 +57,7 @@
     id="dropdown"
     class="z-10 fixed w-72 right-36 top-14 px-2.5 py-2.5 bg-gray-800 divide-y divide-gray-700 rounded-lg shadow"
   >
-    <form on:submit={onSubmit}>
+    <form on:submit={handleSubmit}>
       <div class="mb-4">
         <label
           class="block mb-2 font-medium text-gray-900 dark:text-white"
@@ -61,6 +66,7 @@
         <input
           class="block w-full text-gray-900 border rounded-lg cursor-pointer focus:outline-none bg-gray-700 border-gray-600 placeholder-gray-400"
           id="input"
+          bind:value={$values.file}
           bind:this={input}
           type="file"
         />
