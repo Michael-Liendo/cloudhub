ec806fdb4098e55d7a40cd6ce9f0682fa43c6371
Michael Liendo <michael.m.liendo.r@gmail.com>
2023-06-12 21:37:36 -0400

    feat: create bucket if no exits

--- cloudhub-server/src/index.ts
+++ cloudhub-server/src/index.ts
@@ -6,6 +6,7 @@ import cors from "@fastify/cors";
 import database from "./domain/database";
 import routes from "./routes";
 import fastifyCookie from "@fastify/cookie";
+import { minioClient } from "./domain/minio";
 const fastify = Fastify({
 	bodyLimit: Number(process.env.MAX_FILE_SIZE) * 1024 * 1024,
 });
@@ -28,6 +29,27 @@ database
 		console.error("Unable to connect to the database:", error);
 		process.exit(1);
 	});
+
+minioClient.bucketExists(process.env.BUCKET_NAME, function (err, exists) {
+	if (err) {
+		console.error(err);
+		return;
+	}
+
+	if (exists) {
+		console.log(`Bucket '${process.env.BUCKET_NAME}' already exists.`);
+	} else {
+		// Si el bucket no existe, crea el nuevo bucket
+		minioClient.makeBucket(process.env.BUCKET_NAME, function (err) {
+			if (err) {
+				console.error(err);
+				return;
+			}
+
+			console.log(`Bucket '${process.env.BUCKET_NAME}' created successfully.`);
+		});
+	}
+});
 
 fastify.listen(
 	{ port: Number(process.env.PORT) || 3000 },
